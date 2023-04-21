f562203d4ed8f3465e438197b7331a1db5013c74
Viktor Chen <chenviktor87@gmail.com>
2023-04-21 13:57:22 +0500

    add Zadacha 19

--- Zadacha19/Program.cs
+++ Zadacha19/Program.cs
@@ -11,4 +11,4 @@ void CheckingNumber(string number){
 if (number!.Length == 5){
   CheckingNumber(number);
 }
-//else Console.WriteLine($"Введи правильное число");
+else Console.WriteLine($"Введите правильное число");
